02bbd60e7d566e5981b44f3f9d52e1049527bebd
Van Nieri <pair+van@ministryofvelocity.com>
2021-07-26 12:23:33 -0700

    dislike done

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,5 @@
 class LikesController < ApplicationController
-    before_action :liked, only: [:create]
+    before_action :liked, only: [:create, :destroy]
     def create
         post = Post.find_by(id: params[:post_id])
         post.likes.create(user_id: @body['user_id'])
@@ -8,16 +8,16 @@ class LikesController < ApplicationController
 
     def destroy
         post = Post.find_by(id: params[:post_id])
-        like =  post.likes.find(params[:id])
+        like =  post.likes.find_by(user_id: @body['user_id'])
         like.destroy!
-        render json: {"likes_count": post.like.length}
+        render json: {"likes_count": post.likes.length}
     end
 
     private
     def liked
         @body = JSON.parse(request.body.read)
-        if Like.where(user_id: @body['user_id'], post_id: params[:post_id]).exists?
-            render json: {"message": "already liked"}
-        end
+        # if Like.where(user_id: @body['user_id'], post_id: params[:post_id]).exists?
+        #     render json: {"message": "already liked"}
+        # end
     end
 end
